b0d0d2592bbb5b34e08e5bb6b21d01fbf5cb1f65
lenovo <yusrikualiitam@gmail.com>
2024-09-02 22:20:19 -0700

    valVariable

---
+++ src/main/kotlin/valVarible.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var varvariable: Int
+    varvariable =56
+    println("data pertama ="+varvariable)
+    varvariable = 78
+    println("nilai kedua ="+varvariable)
+    val phi = 3.14
+
+}
