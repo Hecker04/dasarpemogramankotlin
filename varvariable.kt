45105f3452941c5ff231f741d4f4c0c8659b70aa
lenovo <yusrikualiitam@gmail.com>
2024-09-02 22:13:51 -0700

    varVariable

---
+++ varvariable.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var varVariable: int
+    varVariable = 56 // variable pertama intenger
+    println("pemberian nilai pertama pada varVarible +"+varVariable)
+    varVariable = 78 // nilai kedua
+    println("pemberian nilai kedua pada varVariable +"+varVariable)
+
+}
