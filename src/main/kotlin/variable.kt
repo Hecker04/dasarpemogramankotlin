9bbe49bb8148e7b37bab1b542986b08e915f486d
lenovo <yusrikualiitam@gmail.com>
2024-09-02 22:06:28 -0700

    variable

---
+++ src/main/kotlin/variable.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var dynamicVar = 2
+    println("contoh dynamicVar ="+ dynamicVar)
+    var staticVar: Char = 'A'
+    println("contoh static variable =" +staticVar)
+
+}
