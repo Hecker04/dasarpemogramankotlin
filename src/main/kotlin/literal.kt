eab4cb90b8d86959f5015a9c8def1f66e036d719
lenovo <yusrikualiitam@gmail.com>
2024-09-02 21:53:09 -0700

    add literal

---
+++ src/main/kotlin/literal.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    var intkiteral =5
+    var doubleliteral = .02
+    var stringliteral = "yusri"
+    var charliteral = '1'
+    var boolliteral =  true
+
+    println("intliteral ="+intkiteral)
+    println("doubleliteral = "+doubleliteral)
+    println("stringliteral ="+stringliteral)
+    println("charliteral +"+charliteral)
+    println("boolliteral +"+boolliteral)
+
+}
