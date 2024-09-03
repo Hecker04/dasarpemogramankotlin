2bfec7c04723603af8df97e3b731353a8c6ba27c
lenovo <yusrikualiitam@gmail.com>
2024-09-02 21:58:40 -0700

    comen

---
+++ src/main/kotlin/comen.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    println("saya akan membuat contoh komentar atau blok")
+
+    //contoh komentar satu baris
+    /*
+    contoh komentar multi baris
+     */
+    /**
+     * kotlin doc untuk dokumentasi
+     */
+}
